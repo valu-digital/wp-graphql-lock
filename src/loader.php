6d8c5c28a998d0db23a0d2442b3cdd8eb86dd902
Sauli Rajala <sauli.rajala@valu.fi>
2019-07-04 11:31:32 +0300

    Fix typo

--- src/loader.php
+++ src/loader.php
@@ -204,7 +204,7 @@ class Loader {
 				'show_in_graphql'     => $show_in_graphql,
 				'graphql_single_name' => 'persistedQuery',
 				'graphql_plural_name' => 'persistedQueries',
-				'show_ui'             => is_super_dmin(),
+				'show_ui'             => is_super_admin(),
 				'supports'            => [ 'title', 'editor' ],
 			]
 		);
