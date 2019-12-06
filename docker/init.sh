765df3c11a1f2aaeadba045e4b73916e73412260
Esa-Matti Suuronen <esa-matti@suuronen.org>
2019-12-07 00:55:45 +0200

    deps

--- docker/init.sh
+++ docker/init.sh
@@ -16,9 +16,8 @@ if [ -f init-docker.sh ]; then
     exec ./init-docker.sh
 fi
 
-if [ "${WPTT_INSTALL_DIR}" != "" -a -f "${WPTT_INSTALL_DIR}/web/wp-config.php" ]; then
+if [ "$(wp-install --status)" = "full" ]; then
     >&2 echo "WP installed. You can access it from http://localhost:8080/ and run tests against it using ./docker/shell.sh"
-    cd "${WPTT_INSTALL_DIR}/web"
     exec wp-install --serve
 else
     # Otherwise just keep the container running so it can be accessed with docker/shell.sh
